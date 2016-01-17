1b6605ca3498d94d60c7e498c9682fa994f43f56
tstark <tstark@>
2016-01-17 14:19:22 -0500

    Modified executable name and added Sublime project files to gitignore

--- cmake_setup.sh
+++ cmake_setup.sh
@@ -1,17 +1,20 @@
 #/bin/sh
 
+rm -rf local
 mkdir local
 cd local
 cmake -DLOCAL=ON -DCMAKE_BUILD_TYPE=DEBUG ..
 
 cd ..
 
+rm -rf debug
 mkdir debug
 cd debug
 cmake -DCMAKE_BUILD_TYPE=DEBUG ..
 
 cd ..
 
+rm -rf release
 mkdir release
 cd release
 cmake -DCMAKE_BUILD_TYPE=RELEASE ..
