96268528061401dfbae3bb2cd78e4975393dff30
Victor Lai <lai.victor.vl@gmail.com>
2016-01-25 14:27:21 -0500

    Implement K1's first user task

--- src/os/init.c
+++ src/os/init.c
@@ -4,12 +4,50 @@
 #include <bwio/bwio.h>
 
 VOID
+UserTask
+    (
+        VOID
+    )
+{
+    bwprintf(BWCOM2, "UserTask: task id %d\r\n", MyTid());
+
+    Pass();
+
+    bwprintf(BWCOM2, "UserTask: task id %d\r\n", MyTid());
+
+    Exit();
+}
+
+VOID
 InitTask
     (
         VOID
     )
 {
-    bwprintf(BWCOM2, "Init Task Running...\r\n");
+    bwprintf(BWCOM2, "FirstUserTask: starting\r\n");
+
+    int priority = MyTid();
+
+    bwprintf(BWCOM2, "FirstUserTask: task id %d\r\n", priority);
+
+    int userTaskId;
+
+    userTaskId = Create(priority-1, UserTask);
+
+    bwprintf(BWCOM2, "Created: %d\r\n", userTaskId);
+
+    userTaskId = Create(priority-1, UserTask);
+
+    bwprintf(BWCOM2, "Created: %d\r\n", userTaskId);
+
+    userTaskId = Create(priority+1, UserTask);
+
+    bwprintf(BWCOM2, "Created: %d\r\n", userTaskId);
+
+    userTaskId = Create(priority+1, UserTask);
+
+    bwprintf(BWCOM2, "Created: %d\r\n", userTaskId);
+
     bwprintf(BWCOM2, "FirstUserTask: exiting\r\n");
 
     Exit();
