92bb5e2fb24338354427432af72e17c464883d9d
Victor Lai <lai.victor.vl@gmail.com>
2016-02-01 15:17:52 -0500

    Add T_ASSERT method for tests

--- external/rtosc/assert.h
+++ external/rtosc/assert.h
@@ -2,13 +2,6 @@
 
 #include "rt.h"
 
-#ifdef NDEBUG
-
-#define ASSERT(expr, msg)
-#define VERIFY(expr, msg) (expr)
-
-#else
-
 VOID
 assert
     (
@@ -18,7 +11,16 @@ assert
         STRING file
     );
 
+#ifdef NDEBUG
+
+#define ASSERT(expr, msg)
+#define VERIFY(expr, msg) (expr)
+
+#else
+
 #define ASSERT(expr, msg) assert(expr, msg, __LINE__, __FILE__)
 #define VERIFY(expr, msg) ASSERT(expr, msg)
 
 #endif
+
+#define T_ASSERT(expr, msg) assert(expr, msg, __LINE__, __FILE__)
