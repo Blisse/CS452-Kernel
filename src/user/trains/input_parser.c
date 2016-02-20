dafdb0e72976d628d8f46cc1d211c60a6f7acad3
Victor Lai <lai.victor.vl@gmail.com>
2016-02-19 22:03:47 -0500

    Parse keyboard input properly

--- src/user/trains/input_parser.c
+++ src/user/trains/input_parser.c
@@ -10,6 +10,137 @@
 #include "display.h"
 
 static
+VOID
+InputParserpConsumeToken
+    (
+        IN STRING* str,
+        OUT STRING buffer,
+        IN INT bufferLength
+    )
+{
+    CHAR* p = *str;
+    CHAR c;
+    // ignore leading space
+    while ((c = *p++) && isspace(c))
+    {
+    }
+
+    // read until space
+    UINT i = 0;
+    while (!isspace(c) && (i < bufferLength))
+    {
+        buffer[i++] = c;
+        c = *p++;
+    }
+
+    // terminate buffer
+    buffer[i] = '\0';
+
+    // consume token
+    *str = p;
+}
+
+static
+BOOLEAN
+InputParserpIsWhitespace
+    (
+        IN STRING str
+    )
+{
+    CHAR c;
+    while ((c = *str++))
+    {
+        if (!isspace(c))
+        {
+            return FALSE;
+        }
+    }
+    return TRUE;
+}
+
+static
+VOID
+InputParserpParseCommand
+    (
+        IN STRING buffer,
+        IN INT bufferLength
+    )
+{
+    CHAR arg1Buffer[12];
+    CHAR arg2Buffer[12];
+
+    INT arg1;
+    INT arg2;
+
+    CHAR token[12];
+    InputParserpConsumeToken(&buffer, token, sizeof(token));
+
+    bwprintf(BWCOM2, "token : %s \r\n", token);
+    bwprintf(BWCOM2, "buffer : %s \r\n", buffer);
+
+    if (RtStrEqual(token, "tr"))
+    {
+        InputParserpConsumeToken(&buffer, arg1Buffer, sizeof(arg1Buffer));
+        if (RT_SUCCESS(RtAtoi(arg1Buffer, &arg1)))
+        {
+            InputParserpConsumeToken(&buffer, arg2Buffer, sizeof(arg2Buffer));
+            if (RT_SUCCESS(RtAtoi(arg2Buffer, &arg2)))
+            {
+                if (InputParserpIsWhitespace(buffer))
+                {
+                    TrainSetSpeed(arg1, arg2);
+                }
+            }
+        }
+    }
+    else if (RtStrEqual(token, "sw"))
+    {
+        InputParserpConsumeToken(&buffer, arg1Buffer, sizeof(arg1Buffer));
+        if (RT_SUCCESS(RtAtoi(arg1Buffer, &arg1)))
+        {
+            InputParserpConsumeToken(&buffer, arg2Buffer, sizeof(arg2Buffer));
+
+            if ((arg2Buffer[0] == 'S'
+                || arg2Buffer[0] == 's'
+                || arg2Buffer[0] == 'C'
+                || arg2Buffer[0] == 'c')
+                && arg2Buffer[1] == '\0')
+            {
+                SWITCH_DIRECTION direction = SwitchCurved;
+                if (arg2Buffer[0] == 'S' || arg2Buffer[0] == 's')
+                {
+                    direction = SwitchStraight;
+                }
+
+                if (InputParserpIsWhitespace(buffer))
+                {
+                    SwitchSetDirection(arg1, direction);
+                }
+            }
+        }
+    }
+    else if (RtStrEqual(token, "rv"))
+    {
+        InputParserpConsumeToken(&buffer, arg1Buffer, sizeof(arg1Buffer));
+        if (RT_SUCCESS(RtAtoi(arg1Buffer, &arg1)))
+        {
+            if (InputParserpIsWhitespace(buffer))
+            {
+                TrainReverse(arg1);
+            }
+        }
+
+    }
+    else if (RtStrEqual(token, "q"))
+    {
+        if (InputParserpIsWhitespace(buffer))
+        {
+            Shutdown();
+        }
+    }
+}
+
+static
 CHAR
 InputParserpReadChar
     (
@@ -25,32 +156,6 @@ InputParserpReadChar
 
 static
 VOID
-InputParserpReadToken
-    (
-        IN IO_DEVICE* com2Device,
-        OUT STRING buffer,
-        IN INT bufferLength
-    )
-{
-    CHAR c;
-    // ignore leading space
-    while ((c = InputParserpReadChar(com2Device)) && isspace(c))
-    {
-    }
-
-    // read until space
-    UINT i = 0;
-    while (!isspace(c) && i < (bufferLength-1))
-    {
-        buffer[i++] = c;
-        c = InputParserpReadChar(com2Device);
-    }
-
-    buffer[min(i, bufferLength)] = '\0';
-}
-
-static
-VOID
 InputParserpTask
     (
         VOID
@@ -59,63 +164,43 @@ InputParserpTask
     IO_DEVICE com2Device;
     VERIFY(SUCCESSFUL(Open(UartDevice, ChannelCom2, &com2Device)));
 
+    CHAR buffer[256];
+
+    INT i;
+    for (i = 0; i < sizeof(buffer); i++)
+    {
+        buffer[i] = '\0';
+    }
+
     while (1)
     {
-        CHAR buffer[12];
-
-        InputParserpReadToken(&com2Device, buffer, sizeof(buffer));
-
-        if (RtStrEqual(buffer, "tr"))
+        for (i = 0; i < sizeof(buffer); i++)
         {
-            CHAR arg1Buffer[12];
-            CHAR arg2Buffer[12];
+            CHAR c = InputParserpReadChar(&com2Device);
 
-            INT arg1;
-            INT arg2;
-
-            InputParserpReadToken(&com2Device, buffer, sizeof(buffer));
-            if (RT_SUCCESS(RtAtoi(arg1Buffer, &arg1)))
+            if (c == '\r')
             {
-                InputParserpReadToken(&com2Device, buffer, sizeof(buffer));
-                if (RT_SUCCESS(RtAtoi(arg2Buffer, &arg2)))
-                {
-                    TrainSetSpeed(arg1, arg2);
-                }
+                break;
+            }
+            else if (c == '\b')
+            {
+                i--;
+            }
+            else
+            {
+                buffer[i] = c;
             }
         }
-        else if (RtStrEqual(buffer, "sw"))
+
+        INT endIdx = min(i, 255);
+
+        buffer[endIdx] = '\0';
+
+        InputParserpParseCommand(buffer, endIdx);
+
+        while (i > 0)
         {
-            CHAR arg1Buffer[12];
-            CHAR arg2Buffer[12];
-
-            INT arg1;
-            INT arg2;
-
-            InputParserpReadToken(&com2Device, buffer, sizeof(buffer));
-            if (RT_SUCCESS(RtAtoi(arg1Buffer, &arg1)))
-            {
-                InputParserpReadToken(&com2Device, buffer, sizeof(buffer));
-                if (RT_SUCCESS(RtAtoi(arg2Buffer, &arg2)))
-                {
-                    SwitchSetDirection(arg1, arg2);
-                }
-            }
-        }
-        else if (RtStrEqual(buffer, "rv"))
-        {
-            CHAR arg1Buffer[12];
-
-            INT arg1;
-
-            InputParserpReadToken(&com2Device, buffer, sizeof(buffer));
-            if (RT_SUCCESS(RtAtoi(arg1Buffer, &arg1)))
-            {
-                TrainReverse(arg1);
-            }
-        }
-        else if (RtStrEqual(buffer, "q"))
-        {
-            Shutdown();
+            buffer[i--] = '\0';
         }
     }
 }
