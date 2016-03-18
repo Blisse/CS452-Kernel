4b941ecb2bed5b2454aa7372a0a8ef7d820dbc2a
tstark <tstark@>
2016-03-18 16:09:39 -0400

    Fix train reverse bug (I think)

--- src/user/trains/train_server.c
+++ src/user/trains/train_server.c
@@ -1,10 +1,12 @@
 #include "train_server.h"
 
 #include <rtosc/assert.h>
+#include <rtosc/string.h>
 #include <rtkernel.h>
 #include <rtos.h>
 #include <user/trains.h>
 
+#include "display.h"
 #include "location_server.h"
 
 #define TRAIN_SERVER_NAME "train"
@@ -19,7 +21,8 @@ typedef enum _TRAIN_REQUEST_TYPE
     ShutdownRequest = 0,
     SetSpeedRequest,
     GetSpeedRequest,
-    ReverseRequest
+    ReverseRequest, 
+    ReverseStoppedRequest
 } TRAIN_REQUEST_TYPE;
 
 typedef struct _TRAIN_REQUEST
@@ -27,8 +30,13 @@ typedef struct _TRAIN_REQUEST
     TRAIN_REQUEST_TYPE type;
     UCHAR train;
     UCHAR speed;
-    UCHAR* trainSpeed;
 } TRAIN_REQUEST;
+
+typedef struct _TRAIN_WORKER_REQUEST
+{
+    INT delay;
+    TRAIN_REQUEST request;
+} TRAIN_WORKER_REQUEST;
 
 static
 INT
@@ -133,24 +141,44 @@ TrainpReverse
 
 static
 VOID
+TrainpWorkerTask
+    (
+        VOID
+    )
+{
+    // TODO - Something like DelayedSend would be so much better than worker tasks
+    while(1)
+    {
+        INT senderId;
+        TRAIN_WORKER_REQUEST request;
+
+        // Wait for a request from the train server
+        VERIFY(SUCCESSFUL(Receive(&senderId, &request, sizeof(request))));
+        VERIFY(SUCCESSFUL(Reply(senderId, NULL, 0)));
+
+        // Delay for a bit
+        VERIFY(SUCCESSFUL(Delay(request.delay)));
+
+        // Notify the train server
+        VERIFY(SUCCESSFUL(Send(senderId, &request.request, sizeof(request.request), NULL, 0)));
+    }
+}
+
+static
+VOID
 TrainpTask
     (
         VOID
     )
 {
-    IO_DEVICE com1;
-    UCHAR speeds[NUM_TRAINS];
-    INT i;
-    BOOLEAN running = TRUE;
-
-    for(i = 0; i < NUM_TRAINS; i++)
-    {
-        speeds[i] = 0;
-    }
-
-    // Setup the server
+    // Make the server global visible
     VERIFY(SUCCESSFUL(RegisterAs(TRAIN_SERVER_NAME)));
+
+    // On shutdown, stop all trains
     VERIFY(SUCCESSFUL(ShutdownRegisterHook(TrainpShutdownHook)));
+
+    // Open a handle to the train
+    IO_DEVICE com1;
     VERIFY(SUCCESSFUL(Open(UartDevice, ChannelCom1, &com1)));
 
     // Turn the train controller on
@@ -163,6 +191,20 @@ TrainpTask
     VERIFY(SUCCESSFUL(TrainpSetSpeed(&com1, 64, 0)));
     VERIFY(SUCCESSFUL(TrainpSetSpeed(&com1, 68, 0)));
 
+    // Create worker tasks
+    INT workerTasks[MAX_TRACKABLE_TRAINS];
+    UINT nextWorkerTask = 0;
+    for(UINT i = 0; i < MAX_TRACKABLE_TRAINS; i++)
+    {
+        workerTasks[i] = Create(Priority19, TrainpWorkerTask);
+        ASSERT(SUCCESSFUL(workerTasks[i]));
+    }
+
+    // Initialize variables
+    BOOLEAN running = TRUE;
+    UCHAR speeds[NUM_TRAINS];
+    RtMemset(speeds, sizeof(speeds), 0);
+
     while(running)
     {
         INT sender;
@@ -173,22 +215,30 @@ TrainpTask
         switch(request.type)
         {
             case ShutdownRequest:
+            {
                 running = FALSE;
                 VERIFY(SUCCESSFUL(Reply(sender, NULL, 0)));
                 break;
+            }
 
             case GetSpeedRequest:
-                *request.trainSpeed = speeds[request.train - 1];
-                VERIFY(SUCCESSFUL(Reply(sender, NULL, 0)));
+            {
+                UCHAR speed = speeds[request.train - 1];
+                VERIFY(SUCCESSFUL(Reply(sender, &speed, sizeof(speed))));
                 break;
+            }
 
             case SetSpeedRequest:
+            {
+                // Set the train's speed
                 VERIFY(SUCCESSFUL(TrainpSetSpeed(&com1, request.train, request.speed)));
-                VERIFY(SUCCESSFUL(Reply(sender, NULL, 0)));
-                VERIFY(SUCCESSFUL(LocationServerUpdateTrainSpeed(request.train, request.speed)));
-
                 speeds[request.train - 1] = request.speed;
+
+                // Let the location server know the train's new speed
+                VERIFY(SUCCESSFUL(LocationServerUpdateTrainSpeed(request.train, request.speed)));
+                VERIFY(SUCCESSFUL(Reply(sender, NULL, 0)));
                 break;
+            }
 
             case ReverseRequest:
             {
@@ -196,35 +246,58 @@ TrainpTask
 
                 // Stop the train
                 VERIFY(SUCCESSFUL(TrainpSetSpeed(&com1, request.train, 0)));
-                VERIFY(SUCCESSFUL(Reply(sender, NULL, 0)));
+                speeds[request.train - 1] = 0;
+
+                // Let the location server know that the train is stopping
                 VERIFY(SUCCESSFUL(LocationServerUpdateTrainSpeed(request.train, 0)));
 
-                // Wait for the train to come to a stop
-                // TODO - Better implementation
-                Delay(100 * (oldSpeed / 3 + 1));
+                TRAIN_WORKER_REQUEST workerRequest;
+                workerRequest.delay = 100 * (oldSpeed / 3 + 1); // TODO - More accurate stopping time
+                workerRequest.request.type = ReverseStoppedRequest;
+                workerRequest.request.train = request.train;
+                workerRequest.request.speed = oldSpeed;
 
+                INT nextWorkerTaskId = workerTasks[nextWorkerTask % MAX_TRACKABLE_TRAINS];
+                nextWorkerTask++;
+
+                VERIFY(SUCCESSFUL(Send(nextWorkerTaskId, &workerRequest, sizeof(workerRequest), NULL, 0)));
+                VERIFY(SUCCESSFUL(Reply(sender, NULL, 0)));
+                break;
+            }
+
+            case ReverseStoppedRequest:
+            {
                 // Reverse the train
                 VERIFY(SUCCESSFUL(TrainpReverse(&com1, request.train)));
                 VERIFY(SUCCESSFUL(LocationServerFlipTrainDirection(request.train)));
 
-                // TODO - Why do we need this, and only on track B?
-                Delay(10);
+                TRAIN_WORKER_REQUEST workerRequest;
+                workerRequest.delay = 10; // TODO - Why do we need this?
+                workerRequest.request.type = SetSpeedRequest;
+                workerRequest.request.train = request.train;
+                workerRequest.request.speed = request.speed;
 
-                // Speed the train back up to its original speed
-                VERIFY(SUCCESSFUL(TrainpSetSpeed(&com1, request.train, oldSpeed)));
-                VERIFY(SUCCESSFUL(LocationServerUpdateTrainSpeed(request.train, oldSpeed)));
+                INT nextWorkerTaskId = workerTasks[nextWorkerTask % MAX_TRACKABLE_TRAINS];
+                nextWorkerTask++;
+
+                // Since this request was sent to us by a worker task, we should reply before sending
+                // another request to a worker task
+                VERIFY(SUCCESSFUL(Reply(sender, NULL, 0)));
+                VERIFY(SUCCESSFUL(Send(nextWorkerTaskId, &workerRequest, sizeof(workerRequest), NULL, 0)));
                 break;
             }
 
             default:
+            {
                 ASSERT(FALSE);
                 break;
+            }
         }
 
     }
 
     // Stop any trains that are moving
-    for(i = 0; i < NUM_TRAINS; i++)
+    for(UINT i = 0; i < NUM_TRAINS; i++)
     {
         if(0 != speeds[i])
         {
@@ -248,8 +321,17 @@ TrainGetSpeed
         return -1;
     }
 
-    TRAIN_REQUEST request = { GetSpeedRequest, train, 0, speed };
-    return TrainpSendRequest(&request);
+    INT result = WhoIs(TRAIN_SERVER_NAME);
+
+    if(SUCCESSFUL(result))
+    {
+        INT trainServerId = result;
+        TRAIN_REQUEST request = { GetSpeedRequest, train };
+
+        result = Send(trainServerId, &request, sizeof(request), speed, sizeof(*speed));
+    }
+
+    return result;
 }
 
 INT
